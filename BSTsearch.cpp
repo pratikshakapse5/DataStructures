3bd0cf1cf390f2a4a273456db4eca979781b037f
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-15 23:24:29 -0700

    Update BSTsearch.cpp

--- BSTsearch.cpp
+++ BSTsearch.cpp
@@ -1,1 +1,26 @@
 
+//Iteratively search in the binary search tree
+//meaning: keys or values in the nodes are all sorted
+//Search space reduces to half Time complexity: O(logn)
+
+int search(Node* root, int key)
+{
+  
+  if(root==nullptr) //if the tree is empty
+    return nullptr;
+  
+  Node* cur=root;
+  
+  while(cur!=nullptr)
+  {
+   if(key==cur->key)  //found key
+     return cur;
+    else if(key<cur->key) //key is smaller-> search in left sub tree
+      cur=cur->left;
+    else //key is bigger -> search in right sub tree
+      cur=cur->right;
+  }
+  
+  return nullptr; //key not found
+  
+}
