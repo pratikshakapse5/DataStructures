547be910a41d6d35c52ec8dc7a70ce7765ae832f
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-06-15 23:30:01 -0700

    Update BSTinsert.cpp

--- BSTinsert.cpp
+++ BSTinsert.cpp
@@ -2,4 +2,36 @@
 //Search the appropriate node where to insert taking O(logn) time 
 //then insert , space : O(height) - - > worst case O(n)
 
-
+Node* insert(Node* root, int key)
+{
+ 
+  Node* prev=nullptr;
+  Node* cur=root;
+  newNode=new Node(key);
+  
+  //check if the root is nullptr
+  if(root==nullptr)
+    return newNode;
+  
+  while(!cur)
+  {
+    if(key==cur->key)
+      //key alredy exist 
+      print;
+    else if(key<cur->key)
+    {
+     prev=cur;
+      cur=cur->left;
+    }
+    else
+    {
+     prev=cur;
+      cur=cur->right;
+    }
+    
+    return root; //return the whole new tree
+  }
+  
+  
+  
+}
